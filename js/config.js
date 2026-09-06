366b37a358fa96cf77ce0442d4f1919be38b80e5
ZONAQY <zonaqy@gmail.com>
2026-09-06 01:13:54 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 //   - "Project URL"      -> SUPABASE_URL
 //   - "anon public" key  -> SUPABASE_ANON_KEY
 // ============================================================
-const SUPABASE_URL = "COLE_AQUI_A_URL_DO_SEU_PROJETO";
-const SUPABASE_ANON_KEY = "COLE_AQUI_A_CHAVE_ANON_PUBLIC";
+const SUPABASE_URL = "https://hjkmfgnnqhvzktlrumcd.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_kgHpU97i8TC03u_2x_uOIA_FbkUc6O8";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
